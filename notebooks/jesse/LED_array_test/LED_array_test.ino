b7a88171fb0b232e38d4a960490142750b26e315
jgruber5 <112189803+jgruber5@users.noreply.github.com>
2025-04-21 11:38:10 -0500

    Add files via upload

--- notebooks/jesse/LED_array_test/LED_array_test.ino
+++ notebooks/jesse/LED_array_test/LED_array_test.ino
@@ -5,6 +5,7 @@
 #define RED4 36
 #define RED5 35 
 #define button 6
+#define notif 3
 
 int button_cycle = 0;
 
@@ -15,6 +16,7 @@ void setup() {
   pinMode(RED3, OUTPUT);
   pinMode(RED4, OUTPUT);
   pinMode(RED5, OUTPUT);
+  pinMode(notif, OUTPUT);
 
   // DECLARE INPUTS
   pinMode(button, INPUT);
@@ -25,70 +27,71 @@ void setup() {
   digitalWrite(RED3, LOW);
   digitalWrite(RED4, LOW);
   digitalWrite(RED5, LOW);
+  digitalWrite(notif, LOW);
 }
 
 
 
 void loop() {
-int button_state = digitalRead(button);
-  if (button_state = LOW) {
 
-    if (button_cycle = 0) {
       digitalWrite(RED1, LOW);
       digitalWrite(RED2, LOW);
       digitalWrite(RED3, LOW);
       digitalWrite(RED4, LOW);
       digitalWrite(RED5, LOW);
-      button_cycle = 1;
-    }
-    else if (button_cycle = 1) {
+  
+  delay(500);
+
       digitalWrite(RED1, HIGH);
       digitalWrite(RED2, LOW);
       digitalWrite(RED3, LOW);
       digitalWrite(RED4, LOW);
       digitalWrite(RED5, LOW);
-      button_cycle = 2;
-    }
-    else if (button_cycle = 2) {
+
+  delay(500);
+
       digitalWrite(RED1, HIGH);
       digitalWrite(RED2, HIGH);
       digitalWrite(RED3, LOW);
       digitalWrite(RED4, LOW);
       digitalWrite(RED5, LOW);
-      button_cycle = 3;
-    }
-    else if (button_cycle = 3) {
+  
+  delay(500);
+
       digitalWrite(RED1, HIGH);
       digitalWrite(RED2, HIGH);
       digitalWrite(RED3, HIGH);
       digitalWrite(RED4, LOW);
       digitalWrite(RED5, LOW);
-      button_cycle = 4;
-    }
-    else if (button_cycle = 4) {
+   
+  delay(500);
+
       digitalWrite(RED1, HIGH);
       digitalWrite(RED2, HIGH);
       digitalWrite(RED3, HIGH);
       digitalWrite(RED4, HIGH);
       digitalWrite(RED5, LOW);
-      button_cycle = 5;
-    }
-    else if (button_cycle = 5) {
+
+    delay(500);
+
       digitalWrite(RED1, HIGH);
       digitalWrite(RED2, HIGH);
       digitalWrite(RED3, HIGH);
       digitalWrite(RED4, HIGH);
       digitalWrite(RED5, HIGH);
-      button_cycle = 4;
-    }
-  }
-  else {
+ 
+  delay(500);
+
     digitalWrite(RED1, LOW);
     digitalWrite(RED2, LOW);
     digitalWrite(RED3, LOW);
     digitalWrite(RED4, LOW);
     digitalWrite(RED5, LOW);
-  }
 
+  delay(500);
+
+  digitalWrite(notif, HIGH);
+  delay(2000);
+  digitalWrite(notif, LOW);
 
 }
